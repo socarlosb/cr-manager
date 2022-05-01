af5ea8a0d764909a098306dd9491b6ce7aba73ca
Carlos Barbosa <socarlosb@gmail.com>
2022-05-01 01:42:07 +0100

    pwa tweaks

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export function links() {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "CR-Manager",
-  viewport: "width=device-width,initial-scale=1",
+  viewport: "width=device-width,initial-scale=1,user-scalable=no",
 });
 
 export default function App() {
